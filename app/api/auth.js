cadcd6e8a09592dcdf565e10e4655d9dfd899175
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-05-02 17:08:28 +0530

    changes

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,6 +1,6 @@
 import { iron } from "@hapi/iron";
 
-export default async function handler(req, res) {
+export default async function handler(req, res) { 
   if (req.method === "POST") {
     const { username, password } = req.body;
 
