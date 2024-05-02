c7e8cf95c7770bf5a5f0bbba16b16eaed8c318dd
gouravameta71@gmail.com <gouravameta71@gmail.com>
2024-05-02 17:13:46 +0530

    changes

--- app/components/Hero3.tsx
+++ app/components/Hero3.tsx
@@ -5,17 +5,17 @@ const OurGuarantee = () => {
   return (
     <>
       {/* // 2 */}
-      <div class="max-w-4xl mx-auto px-2 mt-5 text-black pb-14 ">
-        <div class="text-center">
-          <h2 class="font-semibold text-4xl">Features you'll love</h2>
-          <p class="max-w-md mx-auto mt-2 text-gray-500">
+      <div className="max-w-4xl mx-auto px-2 mt-5 text-black pb-14 ">
+        <div className="text-center">
+          <h2 className="font-semibold text-4xl">Features you'll love</h2>
+          <p className="max-w-md mx-auto mt-2 text-gray-500">
             Revolutionary Approach to Academic Assistance
           </p>
         </div>
 
-        <div class="grid md:grid-cols-2 gap-12 mt-10">
-          <div class="flex gap-4 items-start">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+        <div className="grid md:grid-cols-2 gap-12 mt-10">
+          <div className="flex gap-4 items-start">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/delivery1.gif"
                 alt="work"
@@ -23,16 +23,16 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">Fast Delivery</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">Fast Delivery</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Swift delivery, exceptional service.
               </p>
             </div>
           </div>
 
-          <div class="flex gap-4 items-start ">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+          <div className="flex gap-4 items-start ">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/support.gif"
                 alt="work"
@@ -40,16 +40,16 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">24/7 Support</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">24/7 Support</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Round-the-clock support: Your needs, our commitment.
               </p>
             </div>
           </div>
 
-          <div class="flex gap-4 items-start">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+          <div className="flex gap-4 items-start">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/plag.gif"
                 alt="work"
@@ -57,8 +57,8 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">Plagiarism free</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">Plagiarism free</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Originality is our pledge, plagiarism-free content our
                 guarantee.
@@ -66,8 +66,8 @@ const OurGuarantee = () => {
             </div>
           </div>
 
-          <div class="flex gap-4 items-start">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+          <div className="flex gap-4 items-start">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/privacy.gif"
                 alt="work"
@@ -75,16 +75,16 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">Privacy</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">Privacy</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Privacy is not an option, it's a necessity.
               </p>
             </div>
           </div>
 
-          <div class="flex gap-4 items-start">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+          <div className="flex gap-4 items-start">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/quality.gif"
                 alt="work"
@@ -92,16 +92,16 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">Quality Work</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">Quality Work</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Excellence in service, quality in work.
               </p>
             </div>
           </div>
 
-          <div class="flex gap-4 items-start">
-            <span class="text-violet-600 bg-violet-500/10 p-3 rounded-full">
+          <div className="flex gap-4 items-start">
+            <span className="text-violet-600 bg-violet-500/10 p-3 rounded-full">
               <img
                 src="/modify.gif"
                 alt="work"
@@ -109,8 +109,8 @@ const OurGuarantee = () => {
               />
             </span>
             <div>
-              <h3 class="font-semibold text-xl">Modification</h3>
-              <p class="mt-1 text-gray-500">
+              <h3 className="font-semibold text-xl">Modification</h3>
+              <p className="mt-1 text-gray-500">
                 {" "}
                 Efficient outsourcing mastery, precision perfected
               </p>
