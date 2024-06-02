01e52e0d7e674c537400d4cf49a8a3aee2a44d26
Gaurav Ameta <gauravameta@Gauravs-Laptop.local>
2024-06-02 15:29:05 +0530

    header bug for phone is fixd

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,39 +1,39 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from "@/firebase";  // Fixed typo from "@/firease" to "@/firebase"
+// import NextAuth from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import { signInWithEmailAndPassword } from 'firebase/auth';
+// import { auth } from "@/firebase";  // Fixed typo from "@/firease" to "@/firebase"
 
-export const authOptions = {
-  pages: {
-    signIn: '/log-in'
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { type: 'text' },
-        password: { type: 'password' }
-      },
-      async authorize(credentials: Record<"email" | "password", string> | undefined, req: any): Promise<any> {
-        if (!credentials) {
-          throw new Error('No credentials provided');
-        }
-        try {
-          const userCredential = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
-          if (userCredential.user) {
-            return userCredential.user;
-          }
-          throw new Error('Authentication failed');
-        } catch (error) {
-          if (error instanceof Error) {
-            throw new Error(`Authentication failed: ${error.message}`);
-          } else {
-            throw new Error('Authentication failed: An unknown error occurred');
-          }
-        }
-      }
-    })
-  ]
-};
+// export const authOptions = {
+//   pages: {
+//     signIn: '/log-in'
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       name: 'Credentials',
+//       credentials: {
+//         email: { type: 'text' },
+//         password: { type: 'password' }
+//       },
+//       async authorize(credentials: Record<"email" | "password", string> | undefined, req: any): Promise<any> {
+//         if (!credentials) {
+//           throw new Error('No credentials provided');
+//         }
+//         try {
+//           const userCredential = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
+//           if (userCredential.user) {
+//             return userCredential.user;
+//           }
+//           throw new Error('Authentication failed');
+//         } catch (error) {
+//           if (error instanceof Error) {
+//             throw new Error(`Authentication failed: ${error.message}`);
+//           } else {
+//             throw new Error('Authentication failed: An unknown error occurred');
+//           }
+//         }
+//       }
+//     })
+//   ]
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
