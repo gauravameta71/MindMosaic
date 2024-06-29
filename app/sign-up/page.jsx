f1a2a33f1711ea041cf025f18f6ff8dcdd606704
Gaurav Ameta <gauravameta@Gauravs-Laptop.local>
2024-06-29 11:46:16 +0530

    issue resolved

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -2,8 +2,8 @@
 import Link from 'next/link';
 import React, { useState } from "react";
 // import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/firebase';
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
+// import { auth } from '@/firebase';/
 
 const page = () => {
   // const [username, setUsername] = useState("");
