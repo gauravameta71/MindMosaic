074296e436e14b1897e27a4243b0a7c6b36b37b9
Gaurav Ameta <gauravameta@Gauravs-Laptop.local>
2024-06-29 11:53:12 +0530

    hosting issue

--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link';
 import React, { useState } from "react";
-// import { useRouter } from "next/navigation";
+
 // import { createUserWithEmailAndPassword } from 'firebase/auth';
 // import { auth } from '@/firebase';/
 
